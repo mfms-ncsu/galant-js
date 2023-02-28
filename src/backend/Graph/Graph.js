aa6cf8dc7ee672bf0b6e0217e23d279f51f6192c
Art Schell <arthurlschell@gmail.com>
2023-02-28 16:26:47 -0500

    Added Rule class

---
+++ src/backend/Graph/Graph.js
@@ -0,0 +1,8 @@
+class Graph {
+    constructor(node, directed, undirected, message) {
+        this.node = node;
+        this.directed = directed;
+        this.undirected = undirected;
+        this.message = message;
+    }
+}
