b1e717a6f1b979352fa5fd9d4690289d08a7f1d8
Noah Alexander <ngalexa2@ncsu.edu>
2023-03-21 15:12:19 -0400

    Class definitions and testing of threads.

--- src/backend/Algorithm/Thread/ThreadHandler.js
+++ src/backend/Algorithm/Thread/ThreadHandler.js
@@ -1,32 +1,64 @@
 const {Worker} = require('worker_threads');
 
-const worker = new Worker('./Thread.js');
-//console.log('running Threadhandler');
-let predicate = {
-    node: {
-      1: {x:3, y:2},
-      2: {x:1, y:2},
-      3: {x:4, y:2}
+export class ThreadHandler {
+    predicate;
+    algorithm;
+    array = new Int32Array(new SharedArrayBuffer(1024));
+    worker;
+    onMessage;
+
+    constructor(predicate, algorithm, onMessage) {
+        this.predicate = predicate;
+        this.algorithm = algorithm;
+        this.onMessage = onMessage;
     }
-};
 
-let buf = new SharedArrayBuffer(1024); 
-let arr = new Int32Array(buf); 
+    startThread() {
+        this.worker = new Worker('./Thread.js');
+        this.worker.postMessage(["shared", array]);
+        this.worker.postMessage(['algorithm/graph', predicate, algorithm]);
+        this.worker.on("message", message => {
+            this.onMessage(message);
+        });
+    }
 
-let algorithm = 'console.log(\'this somehow works \'); api.getNodes();'
-//console.log(algorithm);
-arr[0] = 0;
-console.log("initial value in arr[0]", arr[0]);
-worker.postMessage(["shared", arr]);
+    resumeThread() {
+        Atomics.store(this.arr, 0, 1);
+        Atomics.notify(this.arr, 0);
+    }
 
-console.log("about to change and notify")
-Atomics.store(arr, 0, 1);
-Atomics.notify(arr, 0);
+    killThread() {
+        this.worker.terminate();
+    }
 
-console.log('sending predicate and algorithm now', arr[1]);
-worker.postMessage(['run this thang', predicate, algorithm]);
+    
+}
 
-worker.on("message", message => {
-    //console.log(message);
-});
+// const worker = new Worker('./Thread.js');
+// //console.log('running Threadhandler');
+// let predicate = {
+//     node: {
+//       1: {x:3, y:2},
+//       2: {x:1, y:2},
+//       3: {x:4, y:2}
+//     }
+// };
+
+// let buf = new SharedArrayBuffer(1024); 
+// let arr = new Int32Array(buf); 
+
+// let algorithm = 'let nodes = getNodes(); nodes.forEach(node => colorNode(red, node, arr);'
+
+// worker.postMessage(["shared", arr]);
+
+// worker.postMessage(['algorithm/graph', predicate, algorithm]);
+
+// worker.on("message", message => {
+//     if (message[0]== 'message'){
+//         console.log(message[0] + ' ' + message[1]);
+//     }
+//     else if (message[0] == 'rule') {
+//         console.log(message[0] + ' ' + message[1]);
+//     }
+// });
 
