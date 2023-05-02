d75ad11ac89da92c3718e45ae9407e86461ebfe1
Matthias Stallmann <mfms@ncsu.edu>
2023-05-02 15:26:40 -0400

    added annotations to Collection.jsx

--- src/frontend/Collection/Collection.jsx
+++ src/frontend/Collection/Collection.jsx
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom';
 
 export default function Collection() {
     return <div className="collection">
+        To use these, download them to your device and then upload them from the main Galant window.
         <div className="algorithms">
             <h1>Algorithms</h1>
             <p><Link to="/collection/algorithms/dijkstra.js" target="_blank" download>dijkstra.js</Link> - Dijkstra's algorithm</p>
         </div>
         <div className="graphs">
             <h1>Graphs</h1>
-            <p><Link to="/collection/graphs/weighted_6.txt" target="_blank" download>weighted_6.txt</Link> - A weighted graph</p>
+            <p><Link to="/collection/graphs/weighted_6.txt" target="_blank" download>weighted_6.txt</Link> - A weighted graph with 6 nodes;
+            exhibits different behavior from Dijkstra's algorithm depending on whether directed or undirected.
+            </p>
         </div>
     </div>
 }
