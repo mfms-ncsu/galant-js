8c18ae9d9a172def3e55f6dd1a8a2b2ac7b57c6e
Henry Morris <hrmorri5@ncsu.edu>
2025-01-27 13:26:29 -0500

    Move stylesheet object  back into CytoscapeManager Allows easily modifying values with the scalar.

--- src/graph/CytoscapeManager/CytoscapeManager.js
+++ src/graph/CytoscapeManager/CytoscapeManager.js
@@ -1,5 +1,3 @@
-import stylesheet from "./stylesheet.js";
-
 /**
  * CytoscapeManager is an interface for its graph which returns the graph
  * represented in cytoscape element format. Also generates a stylesheet for
@@ -62,8 +60,8 @@ export default class CytoscapeManager {
             group: "nodes",
             data: { id: node.id },
             position: {
-                x: scalar * node.position.x, // Scale the position
-                y: scalar * node.position.y
+                x: node.position.x, // Scale the position
+                y: node.position.y
             }
         };
 
@@ -100,6 +98,145 @@ export default class CytoscapeManager {
      * @returns Cytoscape stylesheet
      */
     getStyle() {
-        return stylesheet;
+        // Get the scalar for the sizing of elements
+        let scalar = this.#graph.getScalar();
+        
+        return [
+            {
+                "selector": "node",
+                "style": {
+                    "width": `${25 / scalar}px`,
+                    "height": `${25 / scalar}px`,
+                    "backgroundColor": "#FFFFFF",
+                    "color": "#000000",
+                    "borderWidth": `${2.5 / scalar}px`,
+                    "borderStyle": "solid",
+                    "borderColor": "#AAAAAA",
+                    "backgroundOpacity": 1,
+                    "shape": "circle",
+                    "textValign": "center",
+                    "visibility": "visible",
+                    "fontSize": `${12.5 / scalar}px`
+                }
+            },
+            {
+                "selector": "edge",
+                "style": {
+                    "label": "data(id)",
+                    "width": 3 / scalar,
+                    "lineColor": "#444444",
+                    "color": "#AA0000",
+                    "targetArrowColor": "#444444",
+                    "targetArrowShape": "none",
+                    "curveStyle": "bezier"
+                }
+            },
+            {
+                "selector": "node[?marked]",
+                "style": {
+                    "backgroundColor": "orange"
+                }
+            },
+            {
+                "selector": "node[?highlighted]",
+                "style": {
+                    "borderWidth": `${5 / scalar}px`
+                }
+            },
+            {
+                "selector": "node[?invisible]",
+                "style": {
+                    "visibility": "hidden"
+                }
+            },
+            {
+                "selector": "edge[?invisible]",
+                "style": {
+                    "visibility": "hidden"
+                }
+            },
+            {
+                "selector": "edge[?highlighted]",
+                "style": {
+                    "width": `${10 / scalar}px`
+                }
+            },
+            {
+                "selector": "edge[label]",
+                "style": {
+                    "label": "data(label)",
+                    "textWrap": "wrap",
+                    "textBackgroundColor": "white",
+                    "textBackgroundOpacity": "1.0",
+                    "textBackgroundPadding": `${2 / scalar}px`,
+                    "textBorderOpacity": "1.0",
+                    "textBorderStyle": "solid",
+                    "textBorderWidth": `${1 / scalar}px`,
+                    "textBorderColor": "black"
+                }
+            },
+            {
+                "selector": "edge.directed",
+                "style": {
+                    "targetArrowShape": "triangle"
+                }
+            },
+            {
+                "selector": "node[color]",
+                "style": {
+                    "backgroundColor": "data(color)"
+                }
+            },
+            {
+                "selector": "node[id]",
+                "style": {
+                    "label": "data(id)"
+                }
+            },
+            {
+                "selector": "node[shape]",
+                "style": {
+                    "shape": "data(shape)"
+                }
+            },
+            {
+                "selector": "node[borderColor]",
+                "style": {
+                    "borderColor": "data(borderColor)"
+                }
+            },
+            {
+                "selector": "node[borderWidth]",
+                "style": {
+                    "borderWidth": "data(borderWidth)"
+                }
+            },
+            {
+                "selector": "node[backgroundOpacity]",
+                "style": {
+                    "backgroundOpacity": "data(backgroundOpacity)"
+                }
+            },
+            {
+                "selector": "node[size]",
+                "style": {
+                    "width": "data(size)",
+                    "height": "data(size)"
+                }
+            },
+            {
+                "selector": "edge[width]",
+                "style": {
+                    "width": "data(width)"
+                }
+            },
+            {
+                "selector": "edge[color]",
+                "style": {
+                    "lineColor": "data(color)",
+                    "targetArrowColor": "data(color)"
+                }
+            }
+        ];
     }
 }
