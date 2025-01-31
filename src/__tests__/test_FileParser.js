dd6b691616c3e9822b6fb8a7c448c7977c4351df
Henry Morris <hrmorri5@ncsu.edu>
2025-01-31 15:02:56 -0500

    Added file parser tests

---
+++ src/__tests__/test_FileParser.js
@@ -0,0 +1,27 @@
+import Graph from "graph/Graph";
+
+/**
+ * Test methods for the FileParser, which is responsible
+ * for loading a graph from a text file.
+ *
+ * @author Krisjian Smith
+ */
+describe("FileParser tests", () => {
+        
+    /**
+     * Test reading in a few valid files
+     */
+    test("Testing FileParser with a valid file", () => {
+        
+        // TODO: This method
+    });
+    
+    /**
+     * Test reading in invalid files
+     */
+    test("Testing FileParser with invalid files", () => {
+        
+        // TODO: This method
+    });
+    
+});
