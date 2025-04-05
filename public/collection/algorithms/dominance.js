cd13c5101317ae8bb4154961a39378ef42813407
Matthias Stallmann <mfms@ncsu.edu>
2025-04-05 16:42:34 -0400

    happy with dominance

--- public/collection/algorithms/dominance.js
+++ public/collection/algorithms/dominance.js
@@ -11,14 +11,6 @@ let cover = new Set()       // set of nodes in the vertex cover
 let assigned = new Set()    // set of nodes either in cover or out of cover
 
 /**
- * @todo incorporate into Galant JS - returns id of edge between source and target (both as id's)
- *       faking it
- */
-function edgeBetween(source, target) {
-    return source + " " + target
-}
-
-/**
  * Needed to check if queue is empty - must be a better way
  */
 function PQisEmpty() {
@@ -76,49 +68,50 @@ while ( ! PQisEmpty() ) {
         setShape(next_node, "star")
     })
     // @todo use foreach constructs here
-    for ( let neighbor of adjacentNodes(next_node) ) {
+    for ( let neighbor of visibleNeighbors(next_node) ) {
         if ( ! nodePQ[neighbor] ) continue
         display(`Does ${neighbor} dominate ${next_node}?`)
-        // let next_incident = new Set(incident(next_node))
-        // let neighbor_incident = new Set(incident(neighbor))              
-        // let all_incident = next_incident.union(neighbor_incident)
-        let next_adjacent = new Set(adjacentNodes(next_node))
+        let next_incident = new Set(incident(next_node))
+        let neighbor_incident = new Set(incident(neighbor))              
+        let all_incident = next_incident.union(neighbor_incident)
+        let next_adjacent = new Set(visibleNeighbors(next_node))
         next_adjacent.add(next_node)
-        let neighbor_adjacent = new Set(adjacentNodes(neighbor))
+        let neighbor_adjacent = new Set(visibleNeighbors(neighbor))
         neighbor_adjacent.add(neighbor)
         let all_adjacent = next_adjacent.union(neighbor_adjacent)
         step(() => {
             // show edges incident on neighbor and their endpoints
             // distinguish between ones adjacent to both and those not
             for ( let both_adjacent_node of neighbor_adjacent.intersection(next_adjacent) ) {
+                if ( next_node == both_adjacent_node ) continue
                 // nodes adjacent to both
-                let edge_between = edgeBetween(next_node, both_adjacent_node)
+                let edge_between = getEdgeBetween(next_node, both_adjacent_node)
                 console.log("both, node", both_adjacent_node, "edge", edge_between)
-                color(both_adjacent_node, "blue")
+                color(both_adjacent_node, "cyan")
                 highlight(both_adjacent_node)
                 setShape(both_adjacent_node, "star")
-                // color(edge_between, "blue")
-                // highlight(edge_between)
-                // if ( both_adjacent_node == neighbor ) {
-                //     // ends of edge being tested
-                //     color(edge_between, "black")
-                // }
+                color(edge_between, "blue")
+                highlight(edge_between)
+                if ( both_adjacent_node == neighbor ) {
+                    // ends of edge being tested
+                    color(edge_between, "black")
+                }
             }
             for ( let neighbor_only_adjacent_node of neighbor_adjacent.difference(next_adjacent) ) {
-                let edge_between = edgeBetween(neighbor, neighbor_only_adjacent_node)
+                let edge_between = getEdgeBetween(neighbor, neighbor_only_adjacent_node)
                 console.log("neighbor only, node", neighbor_only_adjacent_node, "edge", edge_between)
                 color(neighbor_only_adjacent_node, "yellow")
                 setShape(neighbor_only_adjacent_node, "star")
-                // color(edge_between, "green")
-                // highlight(edge_between)
+                color(edge_between, "green")
+                highlight(edge_between)
             }
             for ( let next_only_adjacent_node of next_adjacent.difference(neighbor_adjacent) ) {
-                let edge_between = edgeBetween(next_node, next_only_adjacent_node)
+                let edge_between = getEdgeBetween(next_node, next_only_adjacent_node)
                 console.log("next only, node", next_only_adjacent_node, "edge", edge_between)
                 color(next_only_adjacent_node, "red")
                 setShape(next_only_adjacent_node, "triangle")
-                // color(edge_between, "red")
-                // highlight(edge_between)
+                color(edge_between, "red")
+                highlight(edge_between)
             }
         })
         if ( next_adjacent.isSubsetOf(neighbor_adjacent) ) {
@@ -128,24 +121,27 @@ while ( ! PQisEmpty() ) {
                     let to_decrease = other(neighbor, edge_to_remove)
                     setWeight(to_decrease, weight(to_decrease) - 1)
                     nodePQ[to_decrease] -= 1
-                    deleteEdge(edge_to_remove)
+                    // hideEdge does not exist in the API
+                    // setAttribute(edge_to_remove, "hidden", true)
                 }
                 // put neighbor into cover
                 cover.add(neighbor)
                 cover_size += 1
-                display("cover size = ", cover_size)
+                console.log("cover size", cover_size)
                 delete nodePQ[neighbor]
-                deleteNode(neighbor)
+                hideNode(neighbor)
             })
+            // it looks like two display calls in the same step don't work
+            // also, display("cover size =", cover_size) appears not to work
+            display(`cover size = ${cover_size}`)
         } // if neighbor dominates
         // clear all animations from this dominance check and blacken node removed from queue
         step(() => {
-            // for ( let incident_edge of all_incident ) {
-            //     unhighlight(incident_edge)
-            //     uncolor(incident_edge)
-            // }
+            for ( let incident_edge of all_incident ) {
+                unhighlight(incident_edge)
+                uncolor(incident_edge)
+            }
             for ( let adjacent_node of all_adjacent ) {
-                if ( adjacent_node == neighbor ) continue
                 unhighlight(adjacent_node)
                 uncolor(adjacent_node)
                 clearShape(adjacent_node)
@@ -153,5 +149,13 @@ while ( ! PQisEmpty() ) {
         })
     } // for each neighbor
     color(next_node, "black")
-    // hideWeight(next_node)
+    hideWeight(next_node)
 } // while nodePQ not empty
+step(() => {
+    for ( let in_cover of cover ) {
+        showNode(in_cover)
+        color(in_cover, "yellow")
+        setShape(in_cover, "star")
+        hideWeight(in_cover)
+    }
+})
