b4d7fd0b48d99bc92d75430c9c5cad01e0e60243
Matthias Stallmann <mfms@ncsu.edu>
2025-07-23 14:53:30 -0400

    changed exit shortcut to 'q'

--- src/pages/Graph/Overlays/AlgorithmControls.jsx
+++ src/pages/Graph/Overlays/AlgorithmControls.jsx
@@ -120,7 +120,7 @@ export default function AlgorithmControls() {
             if (event.target.tagName.toLowerCase() === 'input') return;
             if (event.key === 'ArrowLeft') backButtonPress();
             else if (!event.metaKey && event.key === 'ArrowRight') frontButtonPress();
-            else if (event.key === 'Escape') terminateAlgorithm();
+            else if (event.key === 'q') terminateAlgorithm();
             else if (event.key === 'x') exportGraph();
             else if (event.key === '1') debugMode();
         }
@@ -157,7 +157,7 @@ export default function AlgorithmControls() {
 
             <div className="space-y-1">
                 <PrimaryButton onClick={exportGraph}>Export Graph (x)</PrimaryButton>
-                <ExitButton onClick={terminateAlgorithm}>Exit (esc)</ExitButton>
+                <ExitButton onClick={terminateAlgorithm}>Exit (q)</ExitButton>
             </div>
         </div>
     );
