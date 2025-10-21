dabbd5d27c9f567e9bd8a300843571ef61f1360b
Elizabeth Conger <enconger@ncsu.edu>
2025-10-20 20:21:30 -0400

    removed deletion logic (will handle later, can get from github). Replaced addNode and addNodeHelper with addNodeBST to avoid thread.js fn name collisions and match slide 5. added slide5 fns like isLeaf, isRoot, children, left, right, as well as createDummy, replaceDummy, and dummifyTree. created Weight(x) workaround to try and debug implemetation (thread.js' version is returning undefined for some reason) but will remove later. Adds dummies to tree at the start, and can find where to add, but gets stuck in an infine dummy adding loop, or hits an error. Not working yet, but an improvement and closer to slide5

--- public/collection/algorithms/binarytreebuilder.js
+++ public/collection/algorithms/binarytreebuilder.js
@@ -23,128 +23,163 @@ function getRoot() {
       return x;
     }
   }
+  return undefined;
 }
 
-function addNodeHelper(target, parent){
-  //returns id of a new node. Then do setWeight, then create the edge. Reference slide 5 (make 2 dummies, replace the correct one)
-  if (!getRoot()) {
-    addNodeIdAttrs(target, 0, 0, undefined);
-    display(`Root not found. Added '${target}' as root`);
+//TODO: TEMP FIX. weight in thread.js is undefined, this is a NOT GOOD temp fix. Also breaks detecting adding duplicate nodes
+function weight(x){
+  return x;
+}
+
+function isLeaf(node) {
+  return (outDegree(node) === 0);
+}
+
+function isRoot(node) {
+  return (inDegree(node) === 0);
+}
+
+function children(node){
+  return outgoingNodes(node);
+}
+
+function left(node){
+  return children(node)[0];
+}
+
+function right(node){
+  return children(node)[1];
+}
+
+function createDummy(){
+  const dummy = addNodeIdAttrs(0, 0, 0, 0);
+  setShape(dummy, "square");
+  color(dummy, "black");
+
+  //TODO: fix
+  // Cannot create property 'dummy' on string '0'
+  //dummy.dummy = true;
+  return dummy;
+}
+
+function replaceDummy(parent, dummy, k, side) {
+  deleteNode(dummy); 
+  const newNode = addNodeIdAttrs(k, 0, 0, undefined);
+  setWeight(newNode, k);
+  addEdge(parent, newNode);
+
+  // add 2 dummy children
+  addEdge(newNode, createDummy());
+  addEdge(newNode, createDummy());
+
+  display(`Inserted '${k}' as ${side} child of '${weight(parent)}'`);
+  return newNode;
+}
+
+// A loaded tree does not have dummies. Should add them at the start.
+// Uses up IDs. is that ok???
+function dummifyTree(node) {
+  if (!node){
+    return;
+  } 
+
+  // Skip if node is a dummy
+  if (node.dummy){
     return;
   }
 
-  // Make sure it is added correctly, all cases are accounted for, and the weight is done right
-  addNodeIdAttrs(target, 0 ,0, undefined);
-  addEdge(parent, target);
+  // If leaf, add two dummies
+  if (children(node).length === 0) {
+    addEdge(node, createDummy());
+    addEdge(node, createDummy());
+    return;
+  }
 
+  // Otherwise, recursive call on each child
+  dummifyTree(left(node));
+  dummifyTree(right(node));
+}
+
+
+// Main adding logic
+// Uses same value k for id and weight
+function addNodeBST(x, k) {
+
+  // If empty, make new root
+  if (x === undefined) {
+    const newNode = addNodeIdAttrs(k, 0, 0, undefined);
+    setWeight(newNode, k);
+    addEdge(newNode, createDummy());
+    addEdge(newNode, createDummy());
+    display(`Created root '${k}'`);
+    return;
+  }
+
+  // Show the step to the user
   step(() => {
-    mark(target);
-    highlight(target);
-    label(target, "#" + visit++);
+    mark(x);
+    highlight(x);
+    label(x, "#" + visit++);
   });
 
-  display(`Successully added: '${target}'`);
-}
-
-// Deal with deletion later
-function deleteNodeHelper(target, parent){
-  // Make sure it is removed correctly and all cases are accounted for
-  deleteEdge(getEdgeBetween(parent, target))
-  deleteNode(target);
-  
-  // step(() => {
-  //   mark(target);
-  //   highlight(target);
-  //   label(target, "#" + visit++);
-  // });
-
-  display(`Successully deleted: '${target}'`);
-}
-
-// Recursivly goes through the tree to add the node
-function addNode(node, target, lastVisited) {
-  if (node === undefined) {
-    display(`'${target}' NOT found, adding`);
-    //do the add
-    addNodeHelper(target, lastVisited);
+  // Found duplicate node
+  if (k === weight(x)) {
+    display(`Node with key '${k}' already exists`);
     return;
   }
 
-   step(() => {
-    mark(node);
-    highlight(node);
-    label(node, "#" + visit++);
-  });
+  //TODO: fix infinite loop
+  // If leaf, attach two dummies (tree input will not contain dummies)
+  // Do dummies count towards outgoing edges? How do I know if it is a leaf if it has dummy kids? 
+  // How do I make sure there's only 1 layer of dummies? (adding too many already)
+  // if (isLeaf(x)) {
+  //   addEdge(x, createDummy());
+  //   addEdge(x, createDummy());
+  // }
 
-  // Found the node we are trying to add, error
-  if (node === target) {
-    display(`Cannot add node '${target}', it already exists`);
-    return;
-  }
-
-  // Recursive call
-  const children = outgoingNodes(node);
-  if (weight(target) < weight(node)) {
-    return addNode(children[0], target, node);    
-  } else {
-    return addNode(children[1], target, node);
-  }
- 
-}
-
-// Deal with deletion later
-function deleteNode(node, target, lastVisited) {
-  // Couldn't find node we are trying to delete, error
-  if (node === undefined) {
-    display(`Could not find node '${target}' to delete`);
-    return;
-  }
-
-  step(() => {
-    mark(node);
-    highlight(node);
-    label(node, "#" + visit++);
-  });
-
-  if (node === target) {
-    display(`'${target}' FOUND, deleting`);
-    //do the delete
-    deleteNodeHelper(target, lastVisited);
-    return;
-  }
-
-  // Recursive call
-  const children = outgoingNodes(node);
-  if (weight(target) < weight(node)) {
-    return deleteNode(children[0], target, node);    
-  } else {
-    return deleteNode(children[1], target, node);
+  // At the end of the tree or recursive call
+  if (k < weight(x)) {
+    const L = left(x);
+    if (L && L.dummy) { //end, replace dummy
+      return replaceDummy(x, L, k, "left");
+    } else {
+      return addNodeBST(L, k);  //not end, recur
+    }
+  } else if (k > weight(x)) {
+    const R = right(x);
+    if (R && R.dummy) { //end, replace dummy
+      return replaceDummy(x, R, k, "right");
+    } else {
+      return addNodeBST(R, k);  //not end, recur
+    }
   }
 }
 
+// Entry point
 
-//Entry point
+
+step(() => {
+  dummifyTree(getRoot());
+});
+
 while (!promptBoolean("Is the tree done?")){
   step(() => {
-     learNodeMarks();
+    clearNodeMarks();
     clearNodeHighlights();
     clearNodeLabels();
-    clearNodeWeights();
+    //clearNodeWeights();
     clearEdgeHighlights();
     clearEdgeColors();
     visit = 1;
   });
 
-
-  if (promptBoolean("Would you like to ADD a node")){
-    let weight = prompt("What is the weight of the new node");
-    addNode(getRoot(), weight);
-  // } else if (promptBoolean("Would you like to DELETE a node")){
-  //   let nodeId = prompt("What is the id of the node you would like to delete");
-  //   deleteNode(getRoot(), nodeId);
+  let k = Number(prompt("What is the weight (key) of the new node?"));
+  if (isNaN(k)) {
+    display("Invalid input. Please enter a number.");
   } else {
-    display("Only adding and deleting nodes is supported currently");
+    addNodeBST(getRoot(), k);
   }
+
 }
 
 display("The tree is done; the algorithm is finished");
