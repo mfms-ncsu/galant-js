4681ec46811232d9e87bf4a8630c7275c2e5e497
Matthias Stallmann <mfms@ncsu.edu>
2026-05-27 20:22:17 -0400

    working on rbtree.js with help of slides

--- public/collection/algorithms/rbtree.js
+++ public/collection/algorithms/rbtree.js
@@ -1,18 +1,12 @@
 /**
  * Makes the two children the left and right children of the parent, removing any existing edges
- * @todo make parent a leaf a special case in the rotation functions, of which there need to be several
  */
 function relink(parent, left, right) {
     deleteEdge(parent, getLeft(parent))
     deleteEdge(parent, getRight(parent))
-    if ( ! isLeaf(parent) ) {
-        addEdge(parent, left)
-        setChildren(parent, [left, right])
-    }
-    if ( right !== undefined && right !== null ) {
-        addEdge(parent, right)
-        setChildren(parent, [left, right])
-    }
+    addEdge(parent, left)
+    addEdge(parent, right)
+    setChildren(parent, [left, right])
 }
 
 /**
@@ -29,13 +23,57 @@ function rotate(parents, subtrees) {
     relink(parents[2], subtrees[2], subtrees[3])
 }
 
-/**
- * Helper to rotate if grandparent doesn't 
- * exist and node is parent's left child
- * @param {*} node node we are rotating around the parent
- * @param {*} parent parent of node
- */
-function rotateL(node, parent) {
+function restructure(child, parent, grandparent) {
+    if ( child === getLeft(parent) && parent === getLeft(grandparent) ) {
+        rotate([child, parent, grandparent], [getLeft(child), getRight(child), getRight(parent), getRight(grandparent)])
+        return parent
+    }
+    if ( child === getRight(parent) && parent === getRight(grandparent) ) {
+        rotate([grandparent, parent, child], [getLeft(grandparent), getLeft(parent), getLeft(child), getRight(child)])
+        return parent
+    }
+    if ( child === getLeft(parent) && parent === getRight(grandparent) ) {
+        rotate([grandparent, child, parent], [getLeft(grandparent), getLeft(child), getRight(child), getRight(parent)])
+        return parent
+    }
+    if ( child === getRight(parent) && parent === getLeft(grandparent) ) {
+        rotate([parent, child, grandparent], [getLeft(parent), getLeft(child), getRight(child), getRight(grandparent)])
+        return parent
+    }
 }
 
-// @todo Create a function that handles all the rotation variations, based on relationships between a node, its parent and its grandparent
+/**
+ * Main function that adds a node
+ */
+function addNodeRBT(subroot, newWeight) {
+    if ( isDummy(subroot) ) {
+        // replace subroot with a new red node newNode that has two dummy childen
+        return newNode
+    }
+    let newChildSubroot
+    if ( newWeight < weight(subroot) ) {
+        newChildSubroot = addNodeRBT(getLeft(subroot), newWeight)
+    } else {
+        newChildSubroot = addNodeRBT(getRight(subroot), newWeight)
+    }
+    // if newChildSubroot is black, return the subroot
+    // two possibilities
+    //   - subroot is still black, so double red is impossible
+    //   - subroot has turned red, so any double red will be detected up the line
+
+    // if subroot is the root (re)color it black and return it
+
+    // now we have a double red
+    // newChildSubroot is red and subroot is red
+    const sibling = sibling(subroot)
+    if ( isBlack(sibling) ) {
+        // do a restructure/recolor involving newChildSubroot, subroot, and parent(subroot)
+        // see slide 6 of the lecture slides
+        // then return the root of the restructured subtree
+        //  - this skips a level, but the returned node will be black, so no problem
+    }
+    // now we deal with a red sibling
+    // - see slide 8
+    // recoloring will turn the parent red, which may cause a double red up the line
+    // Note: if parent is the root, it can stay black
+}
